f2ff62267f340d6873eac5d1249f6a3cf1474150
darel <darel.gutierrez12@gmail.com.com>
2023-03-07 23:41:27 +0800

    First commit

---
+++ php/connection.php
@@ -0,0 +1,52 @@
+<?php
+
+$db = new mysqli("localhost", "root", "");
+if ($db->connect_errno > 0) {
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$db->query("CREATE DATABASE IF NOT EXISTS `employee_info`");
+
+mysqli_select_db($db, "employee_info");
+
+$users = "CREATE TABLE IF NOT EXISTS users(id int(3) NOT NULL auto_increment,
+          Username varchar(50),
+          Pass varchar(50),
+          PRIMARY KEY (id))";
+$db->query($users);
+
+$company = "CREATE TABLE IF NOT EXISTS company(id int(6) NOT NULL auto_increment,
+      company_name VARCHAR(50),
+      PRIMARY KEY (id)
+      )";
+$db->query($company);
+
+$deparment = "CREATE TABLE IF NOT EXISTS department(id int(6) NOT NULL auto_increment,
+      company_id int(6),
+      deparment_name VARCHAR(50),
+      PRIMARY KEY (id),
+      FOREIGN KEY (company_id) REFERENCES company(id)
+      )";
+$db->query($deparment);
+
+$position = "CREATE TABLE IF NOT EXISTS positions(id int(6) NOT NULL auto_increment,
+     deparment_id int(6),
+     position_name VARCHAR(50),  
+      PRIMARY KEY (id),
+      FOREIGN KEY (deparment_id) REFERENCES department(id)
+      )";
+$db->query($position);
+
+$employee = "CREATE TABLE IF NOT EXISTS employee(id int(6) NOT NULL auto_increment,
+     position_id INT(6),
+     fname VARCHAR(50),
+     lname VARCHAR(50),
+     loc VARCHAR(50),
+     contact INT(11),
+     birthday DATE,
+     img VARCHAR(50),
+     age INT(2),
+     PRIMARY KEY (id),
+     FOREIGN KEY (position_id) REFERENCES positions(id)     
+    )";
+$db->query($employee);
